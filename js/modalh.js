aeb8d848706657909e9831f40e75299c41419e4b
Raksha <57195964+raksha009@users.noreply.github.com>
2020-09-24 01:43:41 +0530

    Add files via upload

---
+++ js/modalh.js
@@ -0,0 +1,36 @@
+function abcdg(){
+    document.getElementById("popup-9").classList.toggle("active");
+}
+
+function abcdf(){
+    document.getElementById("popup-8").classList.toggle("active");
+}
+
+function abcde(){
+    document.getElementById("popup-7").classList.toggle("active");
+}
+
+
+function guihj(){
+    document.getElementById("popup-6").classList.toggle("active");
+}
+
+function jhhuyt(){
+    document.getElementById("popup-5").classList.toggle("active");
+}
+
+function yrsza(){
+    document.getElementById("popup-4").classList.toggle("active");
+}
+
+function lkmbh(){
+    document.getElementById("popup-3").classList.toggle("active");
+}
+
+function hgsrvm(){
+    document.getElementById("popup-2").classList.toggle("active");
+}
+
+function uijht(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
